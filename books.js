5504d876307df76b5406883f033aa28d787b6e1c
Prerna Mishra <prernamishra2803@gmail.com>
2026-07-03 17:20:25 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,102 @@
+// ————————————————————————————————————————————
+// Prerna's bookshelf
+// To add a book, copy any block and edit the fields.
+// shelf: "currently-reading" or "favorites"
+// image: cover URL (Goodreads image URLs are fine, or any image URL)
+// ————————————————————————————————————————————
+window.BOOKS = [
+  // ─── currently reading ───
+  {
+    shelf: 'currently-reading',
+    title: 'The Vegetarian',
+    author: 'Han Kang',
+    link: 'https://www.goodreads.com/book/show/25489025-the-vegetarian',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1728661771l/25489025._SX98_.jpg'
+  },
+  {
+    shelf: 'currently-reading',
+    title: 'Heart Lamp: Selected Stories',
+    author: 'Banu Mushtaq',
+    link: 'https://www.goodreads.com/book/show/205544315-heart-lamp',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1718702806l/205544315._SX98_.jpg'
+  },
+
+  // ─── favorites ───
+  {
+    shelf: 'favorites',
+    title: 'The God of Small Things',
+    author: 'Arundhati Roy',
+    link: 'https://www.goodreads.com/book/show/9777.The_God_of_Small_Things',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1767305433l/9777._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'Wuthering Heights',
+    author: 'Emily Brontë',
+    link: 'https://www.goodreads.com/book/show/6185.Wuthering_Heights',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388212715l/6185._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'Tuesdays with Morrie',
+    author: 'Mitch Albom',
+    link: 'https://www.goodreads.com/book/show/6900.Tuesdays_with_Morrie',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1423763749l/6900._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'A Thousand Splendid Suns',
+    author: 'Khaled Hosseini',
+    link: 'https://www.goodreads.com/book/show/128029.A_Thousand_Splendid_Suns',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1655336738l/128029._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'Zero to One',
+    author: 'Peter Thiel',
+    link: 'https://www.goodreads.com/book/show/18050143-zero-to-one',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1630663027l/18050143._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'All the Bright Places',
+    author: 'Jennifer Niven',
+    link: 'https://www.goodreads.com/book/show/18460392-all-the-bright-places',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1404331702l/18460392._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'A Little Life',
+    author: 'Hanya Yanagihara',
+    link: 'https://www.goodreads.com/book/show/22822858-a-little-life',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1446469353l/22822858._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'The Seven Husbands of Evelyn Hugo',
+    author: 'Taylor Jenkins Reid',
+    link: 'https://www.goodreads.com/book/show/32620332-the-seven-husbands-of-evelyn-hugo',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1664458703l/32620332._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'The Psychology of Money',
+    author: 'Morgan Housel',
+    link: 'https://www.goodreads.com/book/show/41881472-the-psychology-of-money',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1581527774l/41881472._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'No Rules Rules',
+    author: 'Reed Hastings',
+    link: 'https://www.goodreads.com/book/show/49099937-no-rules-rules',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1595815356l/49099937._SY75_.jpg'
+  },
+  {
+    shelf: 'favorites',
+    title: 'The Almanack of Naval Ravikant',
+    author: 'Eric Jorgenson',
+    link: 'https://www.goodreads.com/book/show/54898389-the-almanack-of-naval-ravikant',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1598011736l/54898389._SY75_.jpg'
+  }
+];
